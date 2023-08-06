f830c36639efe2378824034b59a6fe8e3410fe16
DER-2SH-KA <134935416+DER-2SH-KA@users.noreply.github.com>
2023-08-07 00:13:11 +0700

    Change All Web Site
    Uploaded new Web Site for Start Menu in Browser

--- script.js
+++ script.js
@@ -1,36 +1,18 @@
-let searchQuery = "";
-let themes = ['Персональные компьютеры', 'Периферия', 'Мониторы'];
+// function AddNewWebPage() {
+//     var pageName = prompt("Введите название для новой кнопки");
+//     var pageSource = prompt("Введите ссылку на сайт");
 
-let readInputSearch = function() {
-    searchQuery = String(document.getElementById('inputSearch').value);
+//     const $textNode = document.createTextNode(String(pageName));
+
+//     const $DIV = document.createElement('div');
+//     $DIV.className = "use_link";
+
+//     const $Anchor = document.createElement('a');
+//     $Anchor.appendChild($textNode);
+//     $Anchor.href = String(pageSource);
+
+//     $DIV.appendChild($Anchor);
     
-    if (searchQuery == "") {
-        searchQuery = 'Null';
-    }
-
-    if (searchQuery === "Null") {
-        document.getElementById('inputSearch').style.borderColor = "#f00";
-        setTimeout(() => { 
-            document.getElementById('inputSearch').style.borderColor = "#fff";
-         }, 1000);
-        
-    } else {
-
-        for (let item = 0; item < themes.length;) {
-            if (String(searchQuery) == String(themes[item])) {
-                alert(themes[item]);
-                break;
-            } else {
-                console.log('Нет такой темы');
-                item++;
-            }
-
-            if (item == themes.length) {
-                alert("На данный момент нет статьи на вашу тему...");
-            }
-
-        }
-    }
-
-
-}
+//     const $links = document.querySelector('#links');
+//     $links.appendChild($DIV);
+// }
