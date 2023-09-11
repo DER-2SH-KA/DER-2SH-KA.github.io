84bd806ef0710b87ef459ab09762415ddff3dd5c
DER-2SH-KA <134935416+DER-2SH-KA@users.noreply.github.com>
2023-09-11 17:45:03 +0700

    Big Update. h1 random + d2k + natkButtons
    - h1 tag random title
    - added sites: NATK, Do NATK, D2K
    - CSS edited

--- script.js
+++ script.js
@@ -16,3 +16,19 @@
 //     const $links = document.querySelector('#links');
 //     $links.appendChild($DIV);
 // }
+
+let randomInt = function() {
+    let welcomeText = ["Welcome Back!", "С Возвращением!", "Давно не виделись!", "Здравствуйте!", "Hello!", "Доброго Дня!", "Иисус"];
+    let a = Math.floor(Math.random() * (welcomeText.length - 1));
+    let b = welcomeText[Number(a)];
+    return String(b);
+}
+
+function ChooseTabName() {
+
+    randomInt();
+    
+    document.getElementById('welcomeText').innerHTML = randomInt();
+    console.log("Текст применён")
+
+}
